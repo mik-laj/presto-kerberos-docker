00268c4d873abbbcb4cddc84eb9166c986ffa714
Kamil Breguła <kamil.bregula@polidea.com>
2020-08-17 16:12:28 +0200

    fixup! fixup! fixup! fixup! Initial commit

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 docker-compose run --rm -e KRB5_TRACE=/dev/null machine-example-com bash -c "
 kinit -kt /root/share/kerberos.keytab bob@EXAMPLE.COM &>/dev/null;
-curl -X POST --insecure --negotiate -u : 'https://krb5-presto-example-com:7778/v1/statement' --data 'SELECT 1'
+curl -q -X POST --insecure --negotiate -u : 'https://krb5-presto-example-com:7778/v1/statement' --data 'SELECT 1'
 "
