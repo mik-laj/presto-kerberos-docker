9b98df564193b8c0c86b4c3d9a415e9714d77e0f
Kamil Breguła <kamil.bregula@polidea.com>
2020-08-23 01:29:02 +0200

    Format kaadmin.sh

--- kadmin.sh
+++ kadmin.sh
@@ -2,4 +2,9 @@
 
 set -euo pipefail
 
-docker-compose run -e KRB5_TRACE=/dev/stderr machine-example-com kadmin -p alice/admin@EXAMPLE.COM -w alice
+docker-compose run \
+  -e KRB5_TRACE=/dev/stderr \
+  machine-example-com \
+  kadmin \
+    -p alice/admin@EXAMPLE.COM \
+    -w alice
