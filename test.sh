f44c0ebb543995f080fcbf8c1e682e0df825bac5
Kamil Breguła <kamil.bregula@polidea.com>
2020-08-22 18:07:03 +0200

    Rename presto host to prest-kerberos

--- test.sh
+++ test.sh
@@ -8,5 +8,5 @@ k
 kinit -kt /root/share/client.keytab bob@EXAMPLE.COM
 klist -ktK
 klist
-curl -q -v -X POST --insecure --negotiate -u : 'https://krb5-presto-example-com:7778/v1/statement' --data 'SELECT 1'
+curl -q -v -X POST --insecure --negotiate -u : 'https://presto-kerberos:7778/v1/statement' --data 'SELECT 1'
 "
