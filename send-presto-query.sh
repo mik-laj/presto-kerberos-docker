f79802bdb4d7d3a14d529a822f32d1a93d23add1
Kamil Breguła <mik-laj@users.noreply.github.com>
2021-05-17 23:19:01 +0200

    Wait for query result (#4)

---
+++ send-presto-query.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+function usage() {
+    CMDNAME="$(basename -- "$0")"
+
+      echo """
+Usage: ${CMDNAME} <sql>
+Sends a SQL query to the Presto cluster and waits for the result.
+"""
+}
+
+if [[ ! "$#" -eq 1 ]]; then
+    echo "You must provide exactly one argument."
+    usage
+    exit 1
+fi
+
+
+function send_query {
+  SQL="${1}"
+  RESPONSE="$(docker-compose run --rm -e KRB5_TRACE=/dev/null machine-example-com bash -c "
+  kinit -kt /root/share/kerberos.keytab bob@EXAMPLE.COM &>/dev/null;
+  curl -X POST \
+    --insecure \
+    --negotiate \
+    -u : \
+    'https://krb5-presto-example-com:7778/v1/statement' \
+    --data ${SQL@Q}
+  ")"
+  echo "RESPONSE=${RESPONSE}"
+  NEXT_URL="$(echo "${RESPONSE}"| jq -r .nextUri)"
+  STATE="$(echo "${RESPONSE}" | jq -r .stats.state)"
+
+  echo "${RESPONSE}" | jq .
+
+  while [[ "${STATE}" == "QUEUED" ]]; do
+    RESPONSE="$(docker-compose run --rm -e KRB5_TRACE=/dev/null machine-example-com bash -c "
+    kinit -kt /root/share/kerberos.keytab bob@EXAMPLE.COM &>/dev/null;
+    curl -X GET \
+      --insecure \
+      --negotiate \
+      -u : \
+      ${NEXT_URL@Q}
+    ")"
+    STATE="$(echo "${RESPONSE}" | jq -r .stats.state)"
+    NEXT_URL="$(echo "${RESPONSE}" | jq -r .nextUri)"
+    echo "${RESPONSE}" | jq .
+  done;
+  if [[ "${STATE}" == "FAILED" ]]; then
+    exit 1
+  fi
+  while [[ ! "${NEXT_URL}" == "null" ]]; do
+    RESPONSE="$(docker-compose run --rm -e KRB5_TRACE=/dev/null machine-example-com bash -c "
+    kinit -kt /root/share/kerberos.keytab bob@EXAMPLE.COM &>/dev/null;
+    curl -X GET \
+      --insecure \
+      --negotiate \
+      -u : \
+      ${NEXT_URL@Q}
+    ")"
+    NEXT_URL="$(echo "${RESPONSE}" | jq -r .nextUri)"
+    echo "${RESPONSE}" | jq .
+  done;
+
+  if [[ "${STATE}" == "FAILED" ]]; then
+    exit 1
+  fi
+}
+
+send_query "${1}"
--- test.sh
+++ test.sh
@@ -2,7 +2,4 @@
 
 set -euo pipefail
 
-docker-compose run --rm -e KRB5_TRACE=/dev/null machine-example-com bash -c "
-kinit -kt /root/share/kerberos.keytab bob@EXAMPLE.COM &>/dev/null;
-curl -q -X POST --insecure --negotiate -u : 'https://krb5-presto-example-com:7778/v1/statement' --data 'SELECT 1'
-"
+./send-presto-query.sh "SELECT 1"
