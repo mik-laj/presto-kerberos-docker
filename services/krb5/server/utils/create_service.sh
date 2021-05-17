d87fabcf4bb9a9a8d36990b1a6cddb26908dcc6a
Kamil Breguła <kamilbregula@apache.org>
2021-05-17 23:44:58 +0200

    Use one Common shared volume

---
+++ services/krb5/server/utils/create_service.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+SERVICE_TYPE=$1
+SERVICE_NAME=$2
+KEYTAB_FILE=$3
+
+cat << EOF | kadmin.local
+add_principal -randkey "krb5-${SERVICE_NAME}-example-com.example.com@EXAMPLE.COM"
+add_principal -randkey "${SERVICE_TYPE}/krb5-${SERVICE_NAME}-example-com.example.com@EXAMPLE.COM"
+ktadd -k ${KEYTAB_FILE} -norandkey "krb5-${SERVICE_NAME}-example-com.example.com@EXAMPLE.COM"
+ktadd -k ${KEYTAB_FILE} -norandkey "${SERVICE_TYPE}/krb5-${SERVICE_NAME}-example-com.example.com@EXAMPLE.COM"
+listprincs
+quit
+EOF
+
+chmod 777 "${KEYTAB_FILE}"
