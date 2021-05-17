65f90ba9caf43739fda7411f0f2ef998c1bba97c
Kamil Breguła <mik-laj@users.noreply.github.com>
2021-05-18 00:52:45 +0200

    Improve kerberos utils (#10)

--- services/krb5/server/utils/create_admin.sh
+++ services/krb5/server/utils/create_admin.sh
@@ -5,8 +5,10 @@ set -euo pipefail
 USERNAME=$1
 PASSWORD=$2
 
+REALM_NAME=EXAMPLE.COM
+
 cat << EOF | kadmin.local &>/dev/null
-add_principal -pw $PASSWORD "${USERNAME}/admmin@EXAMPLE.COM"
+add_principal -pw $PASSWORD "${USERNAME}/admmin@${REALM_NAME}"
 listprincs
 quit
 EOF
